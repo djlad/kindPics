39a8b8835074cbbf3c3b89cde35800da7ba479ad
djlad <dynamicdan100@gmail.com>
2017-06-25 13:05:24 -0400

    test

--- kindlePics.py
+++ kindlePics.py
@@ -1,11 +1,12 @@
 import os
-import ImageGrab
+from PIL import ImageGrab
 import pymouse
 import pykeyboard
 import time
+import shutil
 
 def screenShot(coords=[0,0,1,1],name="yo.jpg"):
-	print "picing"
+	print "taking picture"
 	img = ImageGrab.grab(bbox = coords)
 	img.save(folderName+"/"+name)
 	
@@ -14,16 +15,16 @@ def makeFolder(pathName):#param is folder name
 	if not os.path.exists(pathName):
 		os.mkdir(pathName)
 
-
 class boxListener(pymouse.PyMouseEvent):
-	def __init__(self):
+	def __init__(self, pages):
 		self.picBox = [0,0,0,0]
 		self.firstClick = True
 		self.kb = pykeyboard.PyKeyboard()
+		self.pages = pages
 		pymouse.PyMouseEvent.__init__(self)
 	
-	def takeScreenShots(self):
-		for i in range(20):
+	def takeScreenShots(self, pages):
+		for i in range(self.pages):
 			print self.picBox
 			screenShot(self.picBox,str(i)+".jpg")
 			self.kb.tap_key(self.kb.right_key)
@@ -36,19 +37,22 @@ class boxListener(pymouse.PyMouseEvent):
 				self.picBox[0] = x
 				self.picBox[1] = y
 				self.firstClick = False
+				print "click bottom right corner of page."
 			else:
 				self.picBox[2] = x
 				self.picBox[3] = y
-				self.takeScreenShots()
+				self.takeScreenShots(pages)
 
 
 	
 
 if __name__ == "__main__":
-	folderName = "container"
+	folderName = "kindlepicscontainer"
 	makeFolder(folderName)
 
-	screenShot()
-	a = boxListener()
+	pages = raw_input("enter number of pages: ")
+	pages = int(pages)
+	a = boxListener(pages)
+	print "click top left corner of page"
 	a.run()
 	print "this is a thread"
